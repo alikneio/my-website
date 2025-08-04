a3eb2de4790f27247890af68f574b5bab2a792cf
alikneio <alikneio91@gmail.com>
2025-08-04 23:56:12 +0300

    fix: only load dotenv in development

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const { getCachedAPIProducts } = require('./utils/getCachedAPIProducts');
 const sendOrderStatusTelegram = require('./utils/sendOrderStatusTelegram');
 const sendTelegramMessage = require('./utils/sendTelegramNotification');
 const multer = require('multer');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const uploadNone = multer(); // لمعالجة form بدون ملفات
 const PORT = process.env.PORT || 3000;
 console.log("✅ PORT correctly defined:", PORT);
