31767b2dd00655b4d0a746de56f92eeef429fc5b
alikneio <alikneio91@gmail.com>
2025-08-05 00:53:27 +0300

    fix: load dotenv before defining PORT

--- database.js
+++ database.js
@@ -15,6 +15,13 @@ const connection = mysql.createConnection({
     }
   })
 });
+console.log("🔍 ENV VARS:");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 
 connection.connect((err) => {
   if (err) {
