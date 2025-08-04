634d5810c37269dae0e3e43158e0fc65f37c69cf
alikneio <alikneio91@gmail.com>
2025-08-04 16:15:03 +0300

    Updated database connection and added .env

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
 // database.js
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'mysql.railway.internal',
-  user: 'root',
-  password: 'GrYyLrtHsllLcgVUYAsDoZReIwJodGaQ',
-  database: 'railway',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect(err => {
